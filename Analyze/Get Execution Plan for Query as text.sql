8e69ce2036a9aeb00df0fb45f62aa9ec563e377d
mail@peterhenell.se <mail@peterhenell.se>
2014-02-13 13:45:03 +0100

    another test

--- Analyze/Get Execution Plan for Query as text.sql
+++ Analyze/Get Execution Plan for Query as text.sql
@@ -1,8 +1,7 @@
 create function expl(q text) returns setof text as $$
- declare r record;
- begin
-   for r in execute 'explain ' || q loop
-     return next r."QUERY PLAN";
-   end loop;
- end $$ language plpgsql;
- 
+declare r record;
+begin
+	for r in execute 'explain ' || q loop
+		return next r."QUERY PLAN";
+	end loop;
+end $$ language plpgsql;
